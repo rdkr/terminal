853efa9867cd051bafa6db50ac1bec84986c51fe
Neel Radhakrishnan <neel@rdkr.uk>
2024-01-11 11:29:42 +0000

    update list of arch packages

--- aliases-linux.sh
+++ aliases-linux.sh
@@ -2,6 +2,6 @@ function update {
   yay
   antibody bundle < $TERMINAL_DIR/plugins.txt
   antibody update
-  yay -Qe > $TERMINAL_DIR/pacman.txt
+  yay -Qe | cut -f 1 -d ' ' > $TERMINAL_DIR/pacman.txt
   pacman -Qtdq | sudo pacman -Rns -
 }
