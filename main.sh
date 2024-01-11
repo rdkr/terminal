ea26a4f06334101b38810eae5a546d62b22cbd3d
Neel Radhakrishnan <neel@rdkr.uk>
2024-01-11 11:29:42 +0000

    remove mac stuff

--- main.sh
+++ main.sh
@@ -4,48 +4,27 @@ export XDG_CONFIG_HOME="$TERMINAL_DIR/config"
 GOPATH=$HOME/go
 PATH="${GOPATH//://bin:}/bin:$PATH"
 
-#### set zsh config
-DISABLE_AUTO_UPDATE=true
-ZSH="$(antibody path robbyrussell/oh-my-zsh)"
-COMPLETION_WAITING_DOTS="true"
-plugins=(z git kubectl helm gcloud iterm2 fzf gcloud aws)
-
 #### load plugins
 source <(antibody init)
 antibody bundle < $TERMINAL_DIR/plugins.txt
 
-#### set paths / os specific settings
-if [[ "$OSTYPE" == "darwin"* ]]; then
+#### load local imgs
+PATH="$PATH:$TERMINAL_DIR/imgs"
 
-    source $TERMINAL_DIR/aliases-mac.sh
+#### set zsh config
+DISABLE_AUTO_UPDATE=true
+ZSH="$(antibody home)/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh"
+COMPLETION_WAITING_DOTS="true"
+plugins=(z git kubectl helm gcloud iterm2 fzf gcloud aws sudo docker docker-compose)
 
-    # homebrew sbin
-    PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
-
-    # python path
-    PATH="/Users/neel/Library/Python/3.7/bin:$PATH" # "/Users/neel/Library/Python/3.8/bin:$PATH"
-
-    # gnupaths generated with get_gnupaths from aliases.sh
-    PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
-    PATH=/usr/local/opt/gnu-tar/libexec/gnubin:$PATH
-    PATH=/usr/local/opt/grep/libexec/gnubin:$PATH
-    PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH
-    PATH=/usr/local/opt/make/libexec/gnubin:$PATH
-    PATH=/usr/local/opt/findutils/libexec/gnubin:$PATH
-
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-
-    source $TERMINAL_DIR/aliases-linux.sh
-    PATH="$PATH:$TERMINAL_DIR/imgs"
-
-    export GIT_ASKPASS=`which ksshaskpass`
-
-fi
+#### completes
+source <(stern --completion=zsh)
 
 #### load omz
 source $ZSH/oh-my-zsh.sh
 
 #### load terminal custom configs
+source $TERMINAL_DIR/aliases-linux.sh
 source $TERMINAL_DIR/history.sh
 source $TERMINAL_DIR/aliases.sh
 source $TERMINAL_DIR/aws.sh
